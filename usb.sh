667e2106046fe9af08e3c05cbe7fab998a76c9c7
James Chandler <jameschandler750@gmail.com>
2023-12-18 11:12:28 +0000

    Update usb.sh

--- usb.sh
+++ usb.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-echo -------------------------------------------------------------
+#create a while loop with a true condition
 
-USB=$(lsusb | grep DFU | cut -c 34,35,36,37,38)
+while true;do
+ 
+ #seperate each loop of the program by printing dashes
+ 
+ echo "-------------------------------------------------------------"
 
-if [[ $USB != "Apple" ]]; then
+ #find device status by running lsusb and looking for the DFU string in the output and saving it to the variable "USB"
 
-echo "Device is disconnected"
+ USB=$(lsusb | grep DFU | cut -c 34,35,36,37,38)
 
-echo $(date "+%H:%M:%S" && date +%s%3N)
+ #check whether the variable "USB" equals "Apple"
 
-else
+ if [[ $USB == "Apple" ]]; then
+ 
+ #if the variable "USB" equals "Apple" print "Apple, Inc. Mobile Device (DFU Mode)" because the device is connected in DFU Mode
+ 
+ echo "Apple, Inc. Mobile Device (DFU Mode)"
 
-echo "Apple, Inc. Mobile Device (DFU Mode)"
+ #get the hour, minute, second and unix time stamp and print it
 
-echo $(date "+%H:%M:%S" && date +%s%3N)
+ echo $(date "+%H:%M:%S" && date +%s%3N)
 
-fi
+ else
+ 
+ #if the variable "USB" does not equal "Apple" print "Device is disconnected" because the device is not connected in DFU Mode
+ 
+ echo "Device is disconnected" 
 
-./usb.sh
+ #get the hour, minute, second and unix time stamp and print it
+ 
+ echo $(date "+%H:%M:%S" && date +%s%3N)
+
+ fi
+
+done
